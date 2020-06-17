4dfae648e33237602b4e873f1ee6376e80a55655
michael cheng <michaelcheng022@gmail.com>
2020-06-17 14:47:02 -0700

    move to containers && update redux store state

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,32 @@
-import React from 'react'
+
+import React, { useState, useEffect} from 'react'
+import { bindActionCreators } from 'redux'
 import List from './shared/List'
-const Repos = () => {
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
+import { fetchData, creators, fetchActive } from 'store/app/actions';
+
+const Repos = (props) => {
   return (
-   <List sourceType="repos" />
+    <List
+      active={props.active}
+      fetchData={props.fetchData}
+      setActiveRepo={props.setActiveRepo}
+      sourceType="repos"
+    />
   )
 }
 
-export default Repos
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ fetchData, fetchActive, setActiveRepo: creators.setActiveRepo }, dispatch)
+}
+
+const mapStateToProps = state => {
+  return {
+    url: state.app.url,
+    hits: state.app.hits,
+    active: state.app.active,
+    issues: state.app.issues
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Repos);
