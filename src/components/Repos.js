a8a21a2362d0c7dd87d09963fc2c94c136b42092
michael cheng <michaelcheng022@gmail.com>
2020-06-17 16:41:59 -0700

    configure styles for components

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -2,23 +2,29 @@
 import React, { useState, useEffect} from 'react'
 import { bindActionCreators } from 'redux'
 import List from './shared/List'
+import Header from './shared/Header'
+import RepoCard from './RepoCard'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
-import { fetchData, creators, fetchActive } from 'store/app/actions';
+import { fetchData, creators } from 'store/app/actions';
 
 const Repos = (props) => {
   return (
-    <List
-      active={props.active}
-      fetchData={props.fetchData}
-      setActiveRepo={props.setActiveRepo}
-      sourceType="repos"
-    />
+    <div className="list-container">
+      <Header>{"Repositories"}</Header>
+      <List
+        active={props.active}
+        fetchData={props.fetchData}
+        setActiveRepo={props.setActiveRepo}
+        sourceType="repos"
+        card={(repo) => <RepoCard repo={repo} />}
+      />
+    </div>
   )
 }
 
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators({ fetchData, fetchActive, setActiveRepo: creators.setActiveRepo }, dispatch)
+  return bindActionCreators({ fetchData, setActiveRepo: creators.setActiveRepo }, dispatch)
 }
 
 const mapStateToProps = state => {
