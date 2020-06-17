327e1bf4bd00cd29a1d28edb6301c61e5935a9f1
michael cheng <michaelcheng022@gmail.com>
2020-06-17 15:53:02 -0700

    add styles to issue card

--- src/components/Issues.js
+++ src/components/Issues.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect} from 'react'
 import { bindActionCreators } from 'redux'
 import List from './shared/List'
+import IssueCard from './IssueCard'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import { fetchData, creators, fetchActive } from 'store/app/actions';
@@ -11,13 +12,16 @@ const Issues = (props) => {
     <div>
      {!!props.active ?
       <List
-      active={props.active}
-      fetchData={props.fetchData}
-      setActiveRepo={props.setActiveRepo}
-      issuesUrl={props.issuesUrl}
-      sourceType="issues"
-    />
-    : null }
+        active={props.active}
+        fetchData={props.fetchData}
+        setActiveRepo={props.setActiveRepo}
+        issuesUrl={props.issuesUrl}
+        sourceType="issues"
+        card={(issue) => {
+          console.log(issue)
+          return(<IssueCard issue={issue} />)
+      }}
+    /> : null }
     </div>
 
   )
--- src/components/shared/ListItem.js
+++ src/components/shared/ListItem.js
@@ -3,15 +3,12 @@ import PropTypes from 'prop-types'
 import { dispatch } from 'rxjs/internal/observable/pairs'
 
 const ListItem = (props) => {
-  const { sourceType, item, setActive  } = props
-  // console.log(sourceType)
+  const { sourceType, item  } = props
   const fromIssues = sourceType !== 'repos'
-  const onClick = () => {
-   return props.onClick(item)
-  }
+
   return (
-    <div className="repos" onClick={onClick}>
-      {fromIssues ? item.title : item.name}
+    <div className="repos" onClick={() => props.onClick(item)}>
+      {fromIssues ? props.card(item): item.name}
    </div>
   )
 }
