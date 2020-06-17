f436d17de83c6ca06b8c7384cad9fdc6914056ec
michael cheng <michaelcheng022@gmail.com>
2020-06-17 07:25:00 -0800

    create structure for list component

--- src/components/List.js
+++ src/components/List.js
@@ -1,19 +1,48 @@
 import React, { useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
 
-const URL = 'https://api.github.com/user/repos'
+
+const URL = `https://api.github.com/user/michaelcheng022/repos`
+const TEST_URL = 'https://api.github.com/repositories?since=364'
+
+const styles = {
+  listItem: {
+    display: 'flex',
+    margin: 2
+  }
+}
+
+const ListItem = ({ item }) => {
+  return (
+    <div styles={styles.listItem}>
+      {item.name}
+   </div>
+  )
+}
+
 const List = props => {
   const [repos, setRepos] = useState([])
+  const [active, setActive] = useState({})
   useEffect(() => {
-   const data = fetch(URL)
+   fetch(TEST_URL, {
+     method: "GET",
+     headers: new Headers({
+       Accept: 'application/vnd.github.nebula-preview+json'
+     })
+   })
     .then(res=> res.json())
-    .then(data => data)
+    .then(data => setRepos(data))
+    .catch(err => console.log(err))
 
-    return setRepos(data)
     },[])
   return (
-    <div>
-      {'hi'}
+    <div style={{ display: 'flex', flexDirection: 'column'}}>
+      {repos?.length && repos.map((repo, i) => {
+        console.log(repo)
+      return (
+        <ListItem item={repo} />
+      )
+      })}
     </div>
   )
 }
