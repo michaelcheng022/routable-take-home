888f61675ae58e06c7bac0cf4b54c1fb57573070
michael cheng <michaelcheng022@gmail.com>
2020-06-18 11:20:52 -0700

    merge with develop

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import Repos from 'components/Repos/Repos'
 import Issues from 'components/Issues/Issues'
 import 'styles/styles.scss'
@@ -14,22 +13,3 @@ const App = () => {
 }
 
 export default App
-=======
-import Repos from 'components/Repos'
-import Issues from 'components/Issues'
-import 'styles/styles.scss'
-
-function App() {
-  // const { active } = store.getState()
-  return (
-    <div className="App">
-     <Repos />
-     <Issues />
-    </div>
-  );
-}
-
-
-export default App
-// export default connect(mapStateToProps)(App)
->>>>>>> origin/develop
